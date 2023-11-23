c409d94bd06f897fa325315d61afc15059372baf
Alvaro Galiana Albores <algalian@c1r2s2.42madrid.com>
2023-11-23 21:02:01 +0100

    ack

--- client/client.c
+++ client/client.c
@@ -1,18 +1,20 @@
 #include"../minitalk.h"
 
-void num_ok(char *s)
+char *s;
+
+void num_ok(char *str)
 {
 	int i;
 
-	if(ft_atoi(s) <= 0)
+	if(ft_atoi(str) <= 0)
 	{
 		ft_printf("PID must be a positive numeric value. Error 3\n");
 		exit(2);
 	}
 	i = 0;
-	while(s[i])
+	while(str[i])
 	{
-		if(ft_isdigit(s[i]) != 1 )
+		if(ft_isdigit(str[i]) != 1 )
 		{
 			ft_printf("PID must be a positive numeric value\n. Error 4\n");
 			exit(3);
@@ -21,33 +23,30 @@ void num_ok(char *s)
 	}
 }
 
-void signal_seq(pid_t pid, char *s)
+void send_seq(int signum, siginfo_t *info, void *context)
 {
-	int i;
+	static int i;
+	//static int first;
 	
-	i = 0;
-	while(s[i])
+	if(s[i])
 	{
-		while(s[i] >= 1)
+		if(s[i] >= 1)
 		{
 			if((int)s[i] % 2 == 0)
-			{
-				kill(pid, SIGUSR2);
-				usleep(50);
-			}
+				kill(info->si_pid, SIGUSR2);
 			if((int)s[i] % 2 == 1)
-			{
-				kill(pid, SIGUSR1);
-				usleep(50);
-			}
+				kill(info->si_pid, SIGUSR1);
+			ft_printf("%i\n", i);
 			s[i] /= 2;
 		}
 		i++;
-	}
+	}	
 }
+
 int main(int argc, char **argv)
 {
 	pid_t pid;
+	struct sigaction sa;
 
 	if(argc != 3)
 	{
@@ -56,6 +55,17 @@ int main(int argc, char **argv)
 	}
 	pid = ft_atoi(argv[1]);
 	num_ok(argv[1]);
-	signal_seq(pid, argv[2]);
+	s = argv[2];
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &send_seq;
+	sigaction(SIGUSR1, &sa, NULL);
+	if((int)s[0] % 2 == 0)
+		kill(pid, SIGUSR2);
+	if((int)s[0] % 2 == 1)
+		kill(pid, SIGUSR1);
+	while(1)
+	{
+		pause();
+	}
 	return(0);
 }
