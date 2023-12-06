152c26880ad6ec80c02af215d0415db64f666576
Alvaro Galiana Albores <algalian@c1r6s6.42madrid.com>
2023-12-06 18:51:58 +0100

    hines

--- libft/ft_pow.c
+++ libft/ft_pow.c
@@ -1,73 +1,97 @@
-#include"libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pow.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: algalian <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 18:47:28 by algalian          #+#    #+#             */
+/*   Updated: 2023/12/06 18:47:30 by algalian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
 
-static long long ft_basic_pow(int base, int exp)
+static long double	ft_basic_pow(int base, int exp)
 {
-    int i;
-    long long res;
-    
-    i = 1;
-    res = base;
-    while(i < exp)
-    {
-        res = res * base;
-        i += 1;
-    }
-    return(res);
+	int			i;
+	int			x;
+	long double	res;
+
+	i = 1;
+	res = base;
+	x = ft_fabs(exp);
+	while (i < x)
+	{
+		res = res * base;
+		i++;
+	}
+	if (exp < 0)
+		return (1 / res);
+	return (res);
 }
 
-static double ft_float_pow(double base, int exp)
+static double	ft_float_pow(double base, int exp)
 {
-    t_ratio ratio;
-    double long res;
+	t_ratio	ratio;
+	double	res;
 
-    ratio = ft_ratio(base);
-    res = ft_basic_pow(ratio.num, ft_fabs(exp)) / ft_basic_pow(ratio.den, ft_fabs(exp));
-    if(exp < 0)
-        return(1 / res);
-    return(res);
+	ratio = ft_ratio(base);
+	res = ft_basic_pow(ratio.num, ft_fabs(exp))
+		/ ft_basic_pow(ratio.den, ft_fabs(exp));
+	if (exp < 0)
+		return (1 / res);
+	return (res);
 }
 
-static double ft_reverse_pow(double base, double exp)
+static double	ft_reverse_pow(double base, double exp)
 {
-    double rad;
-    double res;
-    t_ratio ratio;
+	double	rad;
+	double	res;
+	t_ratio	ratio;
 
-    ratio = ft_ratio(exp);
-    rad = ft_float_pow(base, ratio.num);
-    res = 0;
-    while(ft_basic_pow(res, ratio.den) < rad)
-        res++;
-    while(ft_float_pow(res, ratio.den) < rad)
-        res += 0.000001;
-    return(res);
+	ratio = ft_ratio(ft_fabs(exp));
+	rad = ft_float_pow(base, ratio.num);
+	res = 0;
+	while (ft_basic_pow(res, ratio.den) < rad)
+		res++;
+	res--;
+	while (ft_float_pow(res, ratio.den) < rad)
+		res += 0.0001;
+	if (exp < 0)
+		return (1 / res);
+	return (res);
 }
 
-double ft_pow(double base, double exp)
+double	ft_pow(double base, double exp)
 {
-    if(exp == 0)
-    {
-        if(base == 0)
-        {
-            ft_printf("0^0 undefined");
-            exit(1);
-        }
-        return(1);
-    }
-    if(ft_truncf(base) < base)
-    {
-        if(ft_truncf(exp) < exp)
-            return(ft_reverse_pow(base, exp));
-        return(ft_float_pow(base, exp));
-    }
-    if(ft_truncf(exp) < exp)
-        return(ft_reverse_pow(base, exp));
-    return(ft_basic_pow(base, exp));
+	double	b;
+	double	x;
+
+	if (exp == 0)
+	{
+		if (base == 0)
+		{
+			write(1, "0^0 undefined", 14);
+			exit(1);
+		}
+		return (1);
+	}
+	b = ft_fabs(base);
+	x = ft_fabs(exp);
+	if (ft_truncf(b) < x)
+	{
+		if (ft_truncf(x) < x)
+			return (ft_reverse_pow(base, exp));
+		return (ft_float_pow(base, exp));
+	}
+	if (ft_truncf(x) < x)
+		return (ft_reverse_pow(base, exp));
+	return (ft_basic_pow(base, exp));
 }
 
 /*int main()
 {
-    double base = 8;
-    double exp  = -2;
-    printf("%f", ft_pow(base, exp));
+	double base = 2.5;
+	double exp  = -3.5;
+	printf("%f", ft_pow(base, exp));
 }*/
